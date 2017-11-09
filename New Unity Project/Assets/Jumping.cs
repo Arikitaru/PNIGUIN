ef531c6a60a1a60779a2022534f25253bbeb6291
Arikitaru <lishchynskyipaul@gmail.com>
2017-11-09 10:57:18 -0700

    v0.1
    Игрок движется, прыгает, камера управляется мышью. Первые твики графики.

---
+++ New Unity Project/Assets/Jumping.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  Jumping : MonoBehaviour 
+{
+	[Range (1, 10)]
+	public float jumpVelocity;
+	public float JumpCooldown;
+	bool canJump = true;
+
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Jump")) 
+			TryJump ();
+	}
+
+	IEnumerator JumpSpamBlock()
+	{
+		canJump = false;
+		yield return new WaitForSeconds (JumpCooldown);
+		canJump = true;
+		yield break;
+	}
+
+	void TryJump()
+	{
+		if (!canJump)
+			return;
+		Jump ();
+		StartCoroutine (JumpSpamBlock ());
+	}
+
+	void Jump()
+	{
+			GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+	}
+}
+
---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	public float speed;
+
+	void Update ()
+	{
+		float mouseInput = Input.GetAxis ("Mouse X");
+		Vector3 lookHere = new Vector3 (0, mouseInput, 0);
+		transform.Rotate (lookHere);
+		transform.Translate (Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+	}
+}
+
+
+
+
+
+
+//	public float speed = 5f;
+//	private Vector3 moveDirection = Vector3.zero;
+//
+//	void Update () 
+//		{
+//			CharacterController controller = GetComponent<CharacterController>();
+//			if (controller.isGrounded)
+//				{
+//					moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+//					moveDirection = Transform.TransformDirection(moveDirection);
+//					moveDirection = Vector3.ClampMagnitude(moveDirection, 1);
+//					moveDirection *= speed;
+//				}
+//
+//		controller.Move(moveDirection * Time.deltaTime);
+//	}
+//}
+
+
+
+
+
+
+
+
+
